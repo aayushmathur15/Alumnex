7f2d111fac480b961fc0d27b1cc4af4c467af4da
Aayush Mathur <mathuraayush15@gmail.com>
2026-02-16 14:05:47 +0000

    Intial Commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth.routes");
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.send("Alumni Portal API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const protect = require("./middleware/auth.middleware");
+const authorizeRoles = require("./middleware/role.middleware");
+
+app.get("/api/test", protect, authorizeRoles("tpo"), (req, res) => {
+  res.json({ message: "TPO access granted" });
+});
