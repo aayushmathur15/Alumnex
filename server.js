9541d362a7b78a12064d93999100441462ae9da5
Aayush Mathur <mathuraayush15@gmail.com>
2026-02-16 17:14:49 +0000

    Done!

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
-dotenv.config();
+
 connectDB();
 
 const app = express();
