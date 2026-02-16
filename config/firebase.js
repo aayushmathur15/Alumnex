853d15bf45d82f30815a3522f45b576199282625
Aayush Mathur <mathuraayush15@gmail.com>
2026-02-16 17:06:45 +0000

    Refactor Firebase configuration to use environment variable for service account

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,14 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseServiceAccount.json");
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is not defined in environment variables");
+}
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
